96f717ac17e15d4e164901f7dba9267135de6137
Jimmy Emery <jimmy.e@outlook.com>
2021-04-08 17:31:01 -0400

    adding tooltip component

--- src/StackedBars/StackedBarsChart.tsx
+++ src/StackedBars/StackedBarsChart.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import cityTemperature from '@visx/mock-data/lib/mocks/cityTemperature';
 import { useTooltip, useTooltipInPortal } from '@visx/tooltip';
+import { TooltipInPortalProps } from '@visx/tooltip/lib/hooks/useTooltipInPortal';
 import AxisBottom from './AxisBottom';
 import Background from './Background';
 import Grid from './Grid';
 import Legend from './Legend';
 import StackedBars from './StackedBars';
 import config from './config';
-import { TooltipData } from './types';
+import { ColorScale, TooltipData } from './types';
 import { formatDate, getDate, getKeys } from './utils';
 import { getColorScale, getDateScale, getTemperatureScale } from './getScales';
 import * as styles from './StackedBars.styles';
+
+interface Props {
+  TooltipInPortal: React.FC<TooltipInPortalProps>;
+  colorScale: ColorScale;
+  data: TooltipData;
+  left: number | undefined;
+  top: number | undefined;
+}
+
+const Tooltip: React.FC<Props> = ({
+  TooltipInPortal,
+  colorScale,
+  data,
+  left,
+  top,
+}) => {
+  return (
+    <TooltipInPortal left={left} top={top} style={styles.tooltipStyle}>
+      <div style={{ color: colorScale(data.key) }}>
+        <strong>{data.key}</strong>
+      </div>
+      <div>{data.bar.data[data.key]}℉</div>
+      <div>
+        <small>{formatDate(getDate(data.bar.data))}</small>
+      </div>
+    </TooltipInPortal>
+  );
+};
 
 const StackedBarsChart: React.FC = () => {
   const data = cityTemperature.slice(0, 12);
@@ -56,18 +85,18 @@ const StackedBarsChart: React.FC = () => {
       </svg>
       <Legend colorScale={colorScale} />
       {tooltipOpen && tooltipData && (
-        <TooltipInPortal top={tooltipTop} left={tooltipLeft} style={styles.tooltipStyle}>
-          <div style={{ color: colorScale(tooltipData.key) }}>
-            <strong>{tooltipData.key}</strong>
-          </div>
-          <div>{tooltipData.bar.data[tooltipData.key]}℉</div>
-          <div>
-            <small>{formatDate(getDate(tooltipData.bar.data))}</small>
-          </div>
-        </TooltipInPortal>
+        <>
+          <Tooltip
+            TooltipInPortal={TooltipInPortal}
+            colorScale={colorScale}
+            data={tooltipData}
+            top={tooltipTop}
+            left={tooltipLeft}
+          />
+        </>
       )}
     </div>
   );
-}
+};
 
 export default StackedBarsChart;

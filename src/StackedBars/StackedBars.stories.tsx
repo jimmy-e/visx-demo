8cfa5b541b6529893fd9a75b688a0dd0337b0055
Jimmy Emery <jimmy.e@outlook.com>
2021-04-08 16:38:17 -0400

    renaming component

--- src/StackedBars/StackedBars.stories.tsx
+++ src/StackedBars/StackedBars.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import StackedBarsComponent from './StackedBars';
+import StackedBarsChartComponent from './StackedBarsChart';
 
 export default {
   title: 'Charts',
 } as Meta;
 
-export const StackedBars: Story = () => <StackedBarsComponent />;
+export const StackedBarsChart: Story = () => <StackedBarsChartComponent />;

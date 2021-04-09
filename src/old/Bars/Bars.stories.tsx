3452cd4dd46c39d0a6dbc9eeb079a75807cefd8b
Jimmy Emery <jimmy.e@outlook.com>
2021-04-09 15:19:27 -0400

    udpating

--- src/old/Bars/Bars.stories.tsx
+++ src/old/Bars/Bars.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import StackedBarsChartComponent from './StackedBarsChart';
+import BarsComponent from './Bars';
 
 export default {
-  title: 'Charts',
+  title: 'Old',
 } as Meta;
 
-export const StackedBarsChart: Story = () => <StackedBarsChartComponent />;
+export const Bars: Story = () => <BarsComponent />;
--- src/old/StackedBars/StackedBars.stories.tsx
+++ src/old/StackedBars/StackedBars.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import BarsComponent from './Bars';
+import StackedBarsChartComponent from './StackedBarsChart';
 
 export default {
-  title: 'Charts',
+  title: 'Old',
 } as Meta;
 
-export const Bars: Story = () => <BarsComponent />;
+export const StackedBarsChart: Story = () => <StackedBarsChartComponent />;

333ca89a6b670622e16138d5fc7eb6f9606ca3ae
Jimmy Emery <jimmy.e@outlook.com>
2021-04-22 13:20:12 -0400

    updating example controls

--- src/XYChart/ExampleControls.tsx
+++ src/XYChart/ExampleControls.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { XYChartProps } from '../types';
 
@@ -9,23 +7,16 @@ type Props = {
   width: number;
 };
 
-export default function ExampleControls({ children, height, width }: Props) {
-  return (
-    <>
-      {children({
-        height,
-        width,
-      })}
-      <div className="controls">
-        <br />
-      </div>
-      <style jsx>{`
-        .pattern-lines {
-          position: absolute;
-          pointer-events: none;
-          opacity: 0;
-        }
-      `}</style>
-    </>
-  );
-}
+const ExampleControls: React.FC<Props> = ({ children, height, width }) => (
+  <>
+    {children({
+      height,
+      width,
+    })}
+    <div className="controls">
+      <br />
+    </div>
+  </>
+);
+
+export default  ExampleControls;

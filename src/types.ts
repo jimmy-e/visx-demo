c6b26ba468ef54a7c59e64070799d663188e49ef
Jimmy Emery <jimmy.e@outlook.com>
2021-04-22 14:15:36 -0400

    udpating types

--- src/types.ts
+++ src/types.ts
@@ -5,26 +5,26 @@ import { curveCardinal, curveLinear, curveStep } from '@visx/curve';
 
 type Accessor = (datum: CityTemperature) => number | string;
 
-interface KeyAccessors {
-  'San Francisco': Accessor;
-  'New York': Accessor;
-  Austin: Accessor;
-}
-
-export type City = 'Austin' | 'New York' | 'San Francisco';
-
-export type DataKey = keyof KeyAccessors;
-
 export interface Accessors {
   x: KeyAccessors;
   y: KeyAccessors;
   date: Accessor;
 }
 
-export type ColorAccessorFactory = (key: DataKey) => (d: CityTemperature) => string | null;
+export type City = 'Austin' | 'New York' | 'San Francisco';
+
+export type ColorAccessorFactory = (dataKey: DataKey) => (datum: CityTemperature) => string | null;
 
 export type Curve = typeof curveLinear | typeof curveCardinal | typeof curveStep;
 
 export type Data = CityTemperature[];
+
+export type DataKey = keyof KeyAccessors;
+
+interface KeyAccessors {
+  'San Francisco': Accessor;
+  'New York': Accessor;
+  Austin: Accessor;
+}
 
 export type RenderGlyph = React.FC<GlyphProps<CityTemperature>>;

afa6bd927319f81b400ad1eeb576d913b81b5df8
Jimmy Emery <jimmy.e@outlook.com>
2021-05-11 17:23:27 -0400

    updating bar stacks story

--- src/viz/molecules/BarStacks/BarStacks.stories.tsx
+++ src/viz/molecules/BarStacks/BarStacks.stories.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { ParentSize } from '@visx/responsive';
 import cityTemperatures from '__fixtures__/cityTemperatures';
-import { CSF, Resizable, Story, StorySection, StoryTemplate } from 'storybook/StoryTemplate';
+import { CSF, Story, StorySection, StoryTemplate } from 'storybook/StoryTemplate';
 import { getDate } from 'organisms/BarStacksChart/utils';
 import BarStacksComponent, { Props } from './BarStacks';
 
 export default CSF('molecules');
 
+const height = 400;
+const width = 400;
+
 const defaultArgs: Props = {
   accessor: getDate,
   data: cityTemperatures,
-  // height,
+  height,
   index: 'date',
-  // width,
+  width,
 };
 
 const expandArgs: Props = {
@@ -23,27 +25,19 @@ const expandArgs: Props = {
 export const BarStacks: Story = () => (
   <StoryTemplate title="Bar Stacks">
     <StorySection title="Default">
-      <Resizable>
-        <ParentSize>
-          {
-            ({ height, width }) => (
-              <svg height={height} width={width}>
-                <BarStacksComponent {...defaultArgs} height={height} width={width} />
-              </svg>
-            )
-          }
-        </ParentSize>
-      </Resizable>
+      <svg height={height} width={width}>
+        <BarStacksComponent {...defaultArgs} />
+      </svg>
     </StorySection>
-    {/*<StorySection title="Offset">*/}
-    {/*  <h1>Auto</h1>*/}
-    {/*  <svg height={height} width={width}>*/}
-    {/*    <BarStacksComponent {...defaultArgs} />*/}
-    {/*  </svg>*/}
-    {/*  <h1>Expand</h1>*/}
-    {/*  <svg height={height} width={width}>*/}
-    {/*    <BarStacksComponent {...expandArgs} />*/}
-    {/*  </svg>*/}
-    {/*</StorySection>*/}
+    <StorySection title="Offset">
+      <h1>Auto</h1>
+      <svg height={height} width={width}>
+        <BarStacksComponent {...defaultArgs} />
+      </svg>
+      <h1>Expand</h1>
+      <svg height={height} width={width}>
+        <BarStacksComponent {...expandArgs} />
+      </svg>
+    </StorySection>
   </StoryTemplate>
 );
